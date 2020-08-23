81dba726be5e8fb92304a009feae26516d63b721
luzy99 <1694957926@qq.com>
2020-08-23 12:11:33 +0800

    Site updated: 2020-08-23 12:11:22

--- js/main.js
+++ js/main.js
@@ -190,7 +190,7 @@
         });
     }
     if (typeof config !== 'undefined'
-        && typeof config.busuanzi_site_offset !== 'undefined' && typeof config.busuanzi == true) {
+        && typeof config.busuanzi_site_offset !== 'undefined' && typeof config.plugins.busuanzi == true) {
         $(document).ready(function () {
             var int = setInterval(fixCount, 100);
             var busuanziSiteOffset = parseInt(config.busuanzi_site_offset);
