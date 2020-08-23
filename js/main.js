69abd8b1f2eafc5eecff2573ed3a10e289fcd15b
luzy99 <1694957926@qq.com>
2020-08-23 12:30:43 +0800

    Site updated: 2020-08-23 12:30:36

--- js/main.js
+++ js/main.js
@@ -189,19 +189,19 @@
             link.target = '_blank';
         });
     }
-
-    if (typeof config !== 'undefined'
-        && typeof config.busuanzi_site_offset !== 'undefined' && typeof config.busuanzi == true) {
-        $(document).ready(function () {
-            var int = setInterval(fixCount, 100);
-            var busuanziSiteOffset = parseInt(config.busuanzi_site_offset);
-            function fixCount() {
-                if ($("#busuanzi_container_site_uv").css("display") != "none") {
-                    clearInterval(int);
-                    $("#busuanzi_value_site_uv").html(parseInt($("#busuanzi_value_site_uv").html()) + busuanziSiteOffset);
-                }
-            }
-        });
-    }
 }(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings));
 
+var config = window.IcarusThemeSettings
+if (typeof config !== 'undefined'
+    && typeof config.busuanzi_site_offset !== 'undefined' && typeof config.busuanzi == true) {
+    $(document).ready(function () {
+        var int = setInterval(fixCount, 100);
+        var busuanziSiteOffset = parseInt(config.busuanzi_site_offset);
+        function fixCount() {
+            if ($("#busuanzi_container_site_uv").css("display") != "none") {
+                clearInterval(int);
+                $("#busuanzi_value_site_uv").html(parseInt($("#busuanzi_value_site_uv").html()) + busuanziSiteOffset);
+            }
+        }
+    });
+}
